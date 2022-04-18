496ca86cb1ba7ad92d2b8e8d5ffe43d7c0283988
Tyler <tyler@webguyty.com>
2022-04-17 17:15:05 -0700

    updates to windows and menus

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const path = require('path')
 const {app, BrowserWindow, Menu} = electron
 
 let mainWindow
+let addWindow
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({})
@@ -15,23 +16,66 @@ app.on('ready', () => {
     slashes: true
   }))
 
+  mainWindow.on('close', () => app.quit())
   const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
   Menu.setApplicationMenu(mainMenu)
 })
+
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: 'Add Item'
+  })
+
+  addWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'addWindow.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  addWindow.on('close', () => addWindow = null)
+}
 
 const mainMenuTemplate = [
   {
     label: 'File',
     submenu: [
       {
-        label: 'Add item'
+        label: 'Add item',
+        click(){
+          createAddWindow()
+        }
       },
       {
         label: 'Clear all items'
       },
       {
-        label: 'Quit'
+        label: 'Quit',
+        accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click(){
+          app.quit()
+        }
       },
     ]
   }
 ]
+
+if (process.platform == 'darwin') {
+  mainMenuTemplate.unshift({})
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  mainMenuTemplate.push({
+    label: 'Developer Tools',
+    submenu: [
+      {
+        label: 'Toggle Dev Tools',
+        accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools()
+        }
+      }
+    ]
+  })
+}
